f95d0905f838887fd4c66a218c083b97416c1956
lekhins <145320131+tastychef@users.noreply.github.com>
2024-11-05 06:56:23 +0300

    Update

--- reminder_bot.py
+++ reminder_bot.py
@@ -224,7 +224,12 @@ def main() -> None:
     )
     application.add_handler(conv_handler)
     application.add_error_handler(error_handler)
-    application.run_polling()
+
+    if os.environ.get('RENDER'):
+        port = int(os.environ.get('PORT', 10000))
+        application.run_webhook(listen="0.0.0.0", port=port, webhook_url=os.environ.get("WEBHOOK_URL"))
+    else:
+        application.run_polling()
 
 
 if __name__ == '__main__':
