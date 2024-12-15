5d6c9fcd9dcc2b27ed3312dd8a38eab7722dff91
lekhins <145320131+tastychef@users.noreply.github.com>
2024-12-15 13:32:29 +0300

    Update

--- reminder_bot - work.py
+++ reminder_bot - work.py
@@ -247,6 +247,7 @@ async def specialist_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         # Отправка списка напоминаний через 10 секунд
         context.job_queue.run_once(send_reminder_list, 10,
                                    data={'projects': specialist['projects'], 'chat_id': query.message.chat.id})
+        # Отправка ближайшей задачи через 20 секунд
         context.job_queue.run_once(send_nearest_task, 20,
                                    data={'projects': specialist['projects'], 'chat_id': query.message.chat.id})
         # Запуск регулярных проверок каждые 48 секунд
@@ -323,15 +324,12 @@ async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_surname = context.user_data.get('surname', 'Неизвестный пользователь')
     update_user_status(update.message.from_user.id, user_surname, "Отключен")
     await update.message.reply_text("Вы отключены от бота. Если захотите снова подключиться, просто напишите /start.")
-def ping_server(context: ContextTypes.DEFAULT_TYPE):
-    # Здесь можно выполнить любое действие, чтобы поддерживать активность
-    logger.info("Ping server to keep it alive")
+
 
 def main() -> None:
     init_db()
     logger.info(f"Бот запущен. Текущее время: {datetime.now(TIMEZONE)}")
     application = Application.builder().token(BOT_TOKEN).build()
-    application.job_queue.run_repeating(ping_server, interval=timedelta(minutes=10))
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
